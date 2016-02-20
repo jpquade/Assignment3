d87e220f3cf9dce513c64ffdf8d25517bba7f5c1
Jeremy Quade <jpquade@gmail.com>
2016-02-19 17:46:53 -0700

    Initial

---
+++ Assignment3/src/cse360assign3/Calculator.java
@@ -0,0 +1,58 @@
+package cse360assign3;
+
+/** Calculator class file
+ * @author Jeremy Quade
+ * @version Feb 19, 2016
+ * 
+ */
+
+
+public class Calculator {
+	
+	// the current total of the calculations
+	private int total;
+	
+	/** Sets total to 0 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	
+	/**  returns the total  */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**  Adds a value to the total
+	 *  @param value	integer used with the operation
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**  subtracts a value to the total
+	 *  @param value	integer used with the operation
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**  multiplies a value into the total
+	 *  @param value	integer used with the operation
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**  divides a value into the total
+	 *  @param value	integer used with the operation
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/** Displays the string of  operators used*/
+	public String getHistory () {
+		return "";
+	}
+}
